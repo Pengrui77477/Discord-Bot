d6c13dd90a4d5712e85b30cbd47542441a2710a3
PR <774772411@qq.com>
2022-01-24 17:56:34 +0800

    test

---
+++ test.js
@@ -0,0 +1,24 @@
+const express =require('express');
+const app=express();
+
+app.listen(3000,()=>{
+    console.log("server is running!");
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hello world!');
+});
+
+const Discord=require("discord.js");
+const client =new Discord.Client({intents:["GUILDS","GUILD_MESSAGES"]});
+
+client.once('ready', () => {
+	console.log('Rob listening at http://localhost:3000');
+});
+client.on("message",message=>{
+    if(message.author.bot) return;
+    if(message.content==='ping'){
+        message.channel.send('pong');
+    }
+})
+client.login('OTI4NDgzMDExMTExMDMwODQ1.YdZbMA.W0RuzIyfu7VVb-a1VVXGklXM-Pc');
