cb91956f0d42c7c02f2ba866c9b3696763de591e
PR <774772411@qq.com>
2022-02-07 10:14:08 +0800

    update

--- service/db/discord_userInfo.js
+++ service/db/discord_userInfo.js
@@ -5,7 +5,7 @@ const userInfo = {};
 userInfo.setInfo = function (params) {
     return new Promise(function (resolve, reject) {
         mysql.getConnection(function (err, connection) {
-            const sql = "insert into discord_info (user_id,guild_id) value (?,?)";
+            const sql = "insert into discord_userinfo (user_id,guild_id) value (?,?)";
             connection.query(sql, [params.user.id, params.guild.id], function (err, result) {
                 if (err) reject(err);
                 resolve(result);
