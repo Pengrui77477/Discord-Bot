d361da7638e4d0c7148946d6fb97c17cca850ffe
PR <774772411@qq.com>
2022-02-09 09:45:49 +0800

    update

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ client.on("messageCreate", async message => {
       )
   }
   if (message.content == ".template") {
-    console.log(GuildTemplate)
+    console.log(client.GuildTemplate)
   }
   if (message.content == ".createguild") {
     const Guild = await client.guilds.create("Test-PlaNFT-Guild", {
