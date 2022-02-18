d26879ce32d5b72e752a48a516bc5080d7b11ffe
PR <774772411@qq.com>
2022-02-18 16:43:17 +0800

    update

--- index.js
+++ index.js
@@ -32,15 +32,16 @@ app.post("/discord/createChannel", async (req, res) => {
   console.log(req.body);
   const data = req.body;
   try {
-    let bot1,bot2 = [];
+    let bot1 = [];
+    let bot2 = [];
     client1.guilds.cache.forEach(async g => {
       bot1.push(g.id);
     });
     // console.log(bot1.length);
-    client2.guilds.cache.forEach(g => {
+    client2.guilds.cache.forEach(async g => {
       bot2.push(g.id);
     })
-    console.log("bot1: " + bot1.length, "bot2: " + bot2.length);
+    // console.log("bot1: " + bot1.length, "bot2: " + bot2.length);
     if (bot1.length < 10) {
       const TemplateGuild = client1.guilds.cache.get('936435431254413392');
       (await TemplateGuild.fetchTemplates()).forEach(async template => {
