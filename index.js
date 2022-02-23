ffa2fd4497f859b67dabfcf2f4cb61b37085257a
PR <774772411@qq.com>
2022-02-23 17:52:30 +0800

    update

--- index.js
+++ index.js
@@ -807,11 +807,17 @@ client.on("messageCreate", async message => {
     const res = message.content.split(" ").reverse();
     const num = res[1];
     let Guild;
-    if (num === 1) Guild = client1.guilds.cache.get(res[0]);
-    if (num === 2) Guild = client2.guilds.cache.get(res[0]);
-    if (num === 3) Guild = client3.guilds.cache.get(res[0]);
-    if (num === 4) Guild = client4.guilds.cache.get(res[0]);
-    if (num === 5) Guild = client5.guilds.cache.get(res[0]);
+    if (num == 1) {
+      Guild = client1.guilds.cache.get(res[0]);
+    } else if (num == 2) {
+      Guild = client2.guilds.cache.get(res[0]);
+    } else if (num == 3) {
+      Guild = client3.guilds.cache.get(res[0]);
+    } else if (num == 4) {
+      Guild = client4.guilds.cache.get(res[0]);
+    } else if (num == 5) {
+      Guild = client5.guilds.cache.get(res[0]);
+    }
     console.log(Guild);
     if (Guild) {
       Guild.delete()
