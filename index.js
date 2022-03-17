85788f2d638ca6dadb855a66e06f2273aefe049a
Pengrui77477 <774772411@qq.com>
2022-03-17 17:48:17 +0800

    update

--- index.js
+++ index.js
@@ -540,63 +540,63 @@ app.post("/discord/discordAuth", async (req, res) => {
 
   const member = Guild.members.cache.get(data.userId);
   const { user_id, guild_id } = await userInfo.getInfo(data);
-  //如果用户存在当前服务器
-  if (member) {
-    //目前简单判断
-    if (user_id === member.id && guild_id === member.guild.id) {
-    // if (data.nftOwner == 1 && user_id === member.user.id && guild_id === member.id) {
-      let role = Guild.roles.cache.find(role => role.name === "[verified]");
-      if (!role) {
-        Guild.roles.create({
-          name: '[verified]',
-          color: '#4fc974',
-          // hoist: true,
-          permissions: [Permissions.FLAGS.VIEW_CHANNEL]
-        }).then(role => {
-          member.roles.add(role);
-        });
-      } else {
-        member.roles.add(role);
-      }
-      //判断用户是否已经拥有角色，避免点击重复发送信息
-      const isRole = member.roles.cache.find(role => role.name === "[verified]");
-      if (!isRole) {
-        embed = new MessageEmbed()
-          .setColor('#f542d4')
-          .setTitle('✅  Verification successful! Now you can chat freely in your guild!')
-          .setTimestamp()
-          .setFooter({ text: 'PlaNFT' });
-        member.send({ embeds: [embed] });
-      }
-      res.send(
-        {
-          code: '200',
-          data,
-          message: "success",
-          status: true
-        }
-      )
-    } else {
-      //判断用户是否已经拥有角色，避免点击重复发送信息
-      const isRole = member.roles.cache.find(role => role.name === "[verified]");
-      if (isRole) return;
+  console.log(user_id, guild_id );
+  // if (member) {
+  //   //目前简单判断
+  //   if (user_id === member.id && guild_id === member.guild.id) {
+  //   // if (data.nftOwner == 1 && user_id === member.user.id && guild_id === member.id) {
+  //     let role = Guild.roles.cache.find(role => role.name === "[verified]");
+  //     if (!role) {
+  //       Guild.roles.create({
+  //         name: '[verified]',
+  //         color: '#4fc974',
+  //         // hoist: true,
+  //         permissions: [Permissions.FLAGS.VIEW_CHANNEL]
+  //       }).then(role => {
+  //         member.roles.add(role);
+  //       });
+  //     } else {
+  //       member.roles.add(role);
+  //     }
+  //     //判断用户是否已经拥有角色，避免点击重复发送信息
+  //     const isRole = member.roles.cache.find(role => role.name === "[verified]");
+  //     if (!isRole) {
+  //       embed = new MessageEmbed()
+  //         .setColor('#f542d4')
+  //         .setTitle('✅  Verification successful! Now you can chat freely in your guild!')
+  //         .setTimestamp()
+  //         .setFooter({ text: 'PlaNFT' });
+  //       member.send({ embeds: [embed] });
+  //     }
+  //     res.send(
+  //       {
+  //         code: '200',
+  //         data,
+  //         message: "success",
+  //         status: true
+  //       }
+  //     )
+  //   } else {
+  //     //判断用户是否已经拥有角色，避免点击重复发送信息
+  //     const isRole = member.roles.cache.find(role => role.name === "[verified]");
+  //     if (isRole) return;
 
-      const embed = new MessageEmbed()
-        .setColor('#f542d4')
-        .setTitle(`❌  Sorry ${member.user.username} , you're not a follower of the NFT`)
-        .setTimestamp()
-        .setFooter({ text: 'PlaNFT' });
-      member.send({ embeds: [embed] })
-      res.send(
-        {
-          code: '200',
-          data,
-          message: "fail",
-          status: false
-        }
-      );
-    }
-  }
+  //     const embed = new MessageEmbed()
+  //       .setColor('#f542d4')
+  //       .setTitle(`❌  Sorry ${member.user.username} , you're not a follower of the NFT`)
+  //       .setTimestamp()
+  //       .setFooter({ text: 'PlaNFT' });
+  //     member.send({ embeds: [embed] })
+  //     res.send(
+  //       {
+  //         code: '200',
+  //         data,
+  //         message: "fail",
+  //         status: false
+  //       }
+  //     );
+  //   }
+  // }
 });
 
 
